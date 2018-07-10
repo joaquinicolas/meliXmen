4849b9d9de5ca26457320b165d02751e9c46ecdc
Joaquin Noe <joako.n96@gmail.com>
2018-07-10 14:40:02 -0300

    Initial commit

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"github.com/joaquinicolas/xmen"
+	"fmt"
+)
+
+type XMen struct {
+	DNA []string `json:"dna" binding:"required"`
+}
+func main() {
+	router := gin.Default()
+	router.POST("/mutant", func(c *gin.Context) {
+		var mutant XMen
+		if err := c.BindJSON(&mutant); err != nil {
+			c.JSON(http.StatusForbidden, gin.H{"error": err})
+			return
+		}
+
+		rowsLength := len(mutant.DNA)
+		colsLength := len(mutant.DNA[0])
+		var dna [][]string
+		for x := 0; x < rowsLength; x++ {
+			var cols []string
+			for i := 0; i < colsLength; i++ {
+				cols = append(cols, string(mutant.DNA[x][i]))
+			}
+			fmt.Println(cols)
+			dna = append(dna, cols)
+		}
+
+		isMutant := xmen.IsMutant(dna)
+		c.JSON(http.StatusOK, gin.H{"isMutant": isMutant})
+	})
+	router.Run()
+}
